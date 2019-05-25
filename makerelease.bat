5fb3d0b5d8dfd49403a97e77fec08491030071d0
SpunkyAardvark123 <ktsanter@gmail.com>
2019-05-25 16:14:55 -0400

    more work on release packaging

--- makerelease.bat
+++ makerelease.bat
@@ -1,18 +1,27 @@
 ::--------------------------------------------------------------------
-:: create release package for Student infoDeck Chrome extension
+:: create release package for Chrome extension
 ::--------------------------------------------------------------------
 @echo off
 setlocal ENABLEDELAYEDEXPANSION
+setlocal 
 
-echo *******************************************************
-echo * building release package for Student infoDeck
-echo *******************************************************
-
-echo.
+set PACKAGE_NAME="Student infoDeck"
+set PACKAGE_RELEASEFILE_PREFIX=sid_release_
 set PACKAGE_VERSION=
 set PACKAGE_RELEASEFILE=
 
+echo *******************************************************
+echo * building release package for %PACKAGE_NAME%
+echo *******************************************************
+echo.
+
+:: initialize log file with time and date
+for /F "tokens=2" %%i in ('date /t') do set mydate=%%i
+set mytime=%time%
+echo time is %mydate% %mytime% > makerelease.log
+
 :: get version string for package from manifest.json
+
 FOR /F "tokens=1,2 delims=:" %%i IN ('find "version" manifest.json') DO (
   set MYTEMPVAR=
   call makerelease_support01 %%i
@@ -23,17 +32,23 @@ FOR /F "tokens=1,2 delims=:" %%i IN ('find "version" manifest.json') DO (
   )
 )
 set MYTEMPVAR=
-if not defined PACKAGE_VERSION goto abort
+
+if not defined PACKAGE_VERSION (
+  echo failed to parse version from manifest.json
+  echo failed to parse version from manifest.json >> makerelease.log 2>&1
+  goto abort
+)
 
 :: fail if release package already exists
-set PACKAGE_RELEASEFILE=sid_release_%PACKAGE_VERSION:~0,-2%.zip
+set PACKAGE_RELEASEFILE=%PACKAGE_RELEASEFILE_PREFIX%%PACKAGE_VERSION:~0,-2%.zip
 if not EXIST %PACKAGE_RELEASEFILE% goto continue01
 echo %PACKAGE_RELEASEFILE% already exists
+echo %PACKAGE_RELEASEFILE% already exists >> makerelease.log 2>&1
 goto abort
 
 :continue01
 echo remove staging directories...
-rmdir release_staging /S /Q  > makerelease.log 2>&1
+rmdir release_staging /S /Q  >> makerelease.log 2>&1
 
 echo creating staging directories...
 mkdir release_staging  >> makerelease.log 2>&1
@@ -55,8 +70,10 @@ cd ..  >> ../makerelease.log 2>&1
 
 
 echo.
-echo release package for Student infoDeck is complete
+echo release package for %PACKAGE_NAME% is complete
+echo release package for %PACKAGE_NAME% is complete >> makerelease.log 2>&1
 echo release file name: %PACKAGE_RELEASEFILE%
+echo release file name: %PACKAGE_RELEASEFILE% >> makerelease.log 2>&1
 echo.
 pause
 goto the_end
@@ -64,11 +81,14 @@ goto the_end
 :abort
 echo.
 echo *** release package build aborted ***
+echo *** release package build aborted *** >> makerelease.log 2>&1
 echo.
 pause
 goto the_end
 
 :the_end
+set PACKAGE_NAME=
+set PACKAGE_RELEASEFILE_PREFIX=
 set PACKAGE_VERSION=
 set PACKAGE_RELEASEFILE=
 exit /B
